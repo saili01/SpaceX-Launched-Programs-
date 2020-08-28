7464f6c1913df541bceb35a0382819680ce90f94
Saili Manchkar <46997364+saili01@users.noreply.github.com>
2020-08-28 10:21:42 +0530

    Delete spacexdata.js

--- src/Api/spacexdata.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL:'https://api.spacexdata.com/v3/launches?limit=100',
-    // headers:{
-    //     Authorization: 'Client-ID _w7kRrYkf6aEAZCB8UiThCipu2OgR51UprZkMBNmRfQ',
-    // }
-});
