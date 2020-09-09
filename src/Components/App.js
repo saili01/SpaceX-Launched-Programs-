df13a928423ad7732ee41be32a817797cb8cdc28
saili01 <saili25manchkar@gmail.com>
2020-09-09 21:00:05 +0530

    Updated

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import LandingPage from './LandingPage';
+import ReactGa from "react-ga";
 
 
 class App extends React.Component{
-    
+
+    componentDidMount(){
+        ReactGa.initialize('UA-177534020-1');
+        ReactGa.pageview('/');
+    }
+    componentDidUpdate(){
+        ReactGa.initialize('UA-177534020-1');
+        ReactGa.pageview('/');
+    }
+
     render(){
         return (
             <div>
